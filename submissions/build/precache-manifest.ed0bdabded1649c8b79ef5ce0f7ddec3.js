ea1307e6ffd559a22bf65b1d81d909e5b6bcfbe4
Nir Parisian <nir.parisian@gmail.com>
2019-04-04 00:36:23 +0300

    final

---
+++ submissions/build/precache-manifest.ed0bdabded1649c8b79ef5ce0f7ddec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "654dc8fe03abbc03f4c8",
+    "url": "/static/js/main.cd7aabbb.chunk.js"
+  },
+  {
+    "revision": "a3d941cfe047e34421b6",
+    "url": "/static/js/2.d4778ae5.chunk.js"
+  },
+  {
+    "revision": "654dc8fe03abbc03f4c8",
+    "url": "/static/css/main.6403025d.chunk.css"
+  },
+  {
+    "revision": "ac756d84ed470667fcf48b5bfacf31f8",
+    "url": "/index.html"
+  }
+];
